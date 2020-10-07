8feab4a1bf593ee6d3ce56949fee643f304035dc
jmanuel-cool <jmanuel-cool@users.noreply.github.com>
2020-10-07 10:28:42 -0400

    Update reproductor.sh

--- reproductor.sh
+++ reproductor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+##Requiere Zenity o Yad
+##apt install zenity yad
 #song=$(yad --width=360 --height=320 --title "Select Folder" --file-selection --directory $HOME)
 song=$(zenity --width=360 --height=320 --title "Select Folder" --file-selection --directory $HOME)
+##Requiere mpg123 o mpv
 #find "$song" -name "*.mp3" | sort --random-sort | head -n 100 | xargs -d '\n' mpg123
 find "$song" -name "*.mp3" | sort --random-sort | head -n 100 | xargs -d '\n' mpv --no-audio-display
